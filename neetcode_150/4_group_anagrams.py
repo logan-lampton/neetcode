397d19bbfe5e14fbbd2081236c9b26a8122726f3
logan-lampton <lampton.logan@gmail.com>
2023-07-15 19:18:47 -0400

    solution to #5

--- neetcode_150/4_group_anagrams.py
+++ neetcode_150/4_group_anagrams.py
@@ -1,3 +1,8 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all
+# the original letters exactly once.
+
 class Solution(object):
     def groupAnagrams(self, strs):
         # hash map
---
+++ neetcode_150/5_top_k_frequent_elements.py
@@ -0,0 +1,23 @@
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in
+# any order.
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        # use bucket sort
+        # hash map:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            #  the count.get will return 0, if there is the num isn't already in the hash map
+            count[num] = 1 + count.get(num, 0)
+        for num, char in count.items():
+            # num appears char times
+            freq[char].append(num)
+        res = []
+        # length of freq, starting at the last index, going to 0 index, decrementing -1
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
