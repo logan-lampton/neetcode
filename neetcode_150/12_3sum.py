913714f47b841dd50c3fe32629053ef185e44e38
logan-lampton <lampton.logan@gmail.com>
2023-07-23 14:25:53 -0400

    solution to #12

---
+++ neetcode_150/12_3sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def threeSum(self, nums):
+        output = []
+        nums.sort()
+        for i, num in enumerate(nums):
+            if i > 0 and num == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = num + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    output.append([num, nums[l], nums[r]])
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return output
