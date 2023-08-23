f21f5d6b4429b88420245066a8b08ddb44d1e42f
logan-lampton <lampton.logan@gmail.com>
2023-08-23 12:18:29 -0400

    solved Leetcode #1732

---
+++ leetcode/1732_Find_the_Highest_Altitude.py
@@ -0,0 +1,32 @@
+# There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
+# The biker starts his trip on point 0 with altitude equal 0.
+
+# You are given an integer array gain of length n where gain[i] is the net gain in altitude between points
+# i and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+# Example 1:
+# Input: gain = [-5,1,5,0,-7]
+# Output: 1
+# Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+# Example 2:
+# Input: gain = [-4,-3,-2,-1,4,3,2]
+# Output: 0
+# Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        heighest_point = 0
+        altitude = [0]
+
+        if heighest_point == 0:
+            altitude.append(gain[0])
+
+        for i in range(1, len(gain)):
+            new_altitude = altitude[-1] + gain[i]
+            altitude.append(new_altitude)
+
+        for a in altitude:
+            heighest_point = max(heighest_point, a)
+
+        return heighest_point
