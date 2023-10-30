3026fd1a541dd2192e4fc2f7e9f2ade35a1d68a1
logan-lampton <lampton.logan@gmail.com>
2023-10-30 11:41:53 -0400

    solved Leetcode #205

---
+++ leetcode/205_Isomorphic_Strings.py
@@ -0,0 +1,43 @@
+# Given two strings s and t, determine if they are isomorphic.
+
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+# Example 1:
+# Input: s = "egg", t = "add"
+# Output: true
+
+# Example 2:
+# Input: s = "foo", t = "bar"
+# Output: false
+
+# Example 3:
+# Input: s = "paper", t = "title"
+# Output: true
+
+# Constraints:
+# 1 <= s.length <= 5 * 104
+# t.length == s.length
+# s and t consist of any valid ascii character.
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        memo_s = {}
+        memo_t = {}
+
+        for i in range(len(s)):
+            if s[i] not in memo_s:
+                memo_s[s[i]] = t[i]
+            else:
+                if memo_s[s[i]] != t[i]:
+                    return False
+            
+            if t[i] not in memo_t:
+                memo_t[t[i]] = s[i]
+            else:
+                if memo_t[t[i]] != s[i]:
+                    return False
+        
+        return True
