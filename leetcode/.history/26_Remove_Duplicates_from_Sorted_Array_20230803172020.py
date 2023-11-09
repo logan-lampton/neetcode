edcc308c4ac0b0fb715c49f6ef99a1cb960d63c9
logan-lampton <lampton.logan@gmail.com>
2023-11-09 13:21:40 -0500

    addtional method to solving Leetcode #26

--- leetcode/.history/26_Remove_Duplicates_from_Sorted_Array_20230803172020.py
+++ leetcode/.history/26_Remove_Duplicates_from_Sorted_Array_20230803172020.py
@@ -16,31 +16,15 @@
 # Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
-
 class Solution(object):
     def removeDuplicates(self, nums):
-        # too simple:
         # nums.sort()
-
-        # method 1:
-        # i = 0
-        # while i < len(nums) - 1:
-        #     if nums[i] == nums[i + 1]:
-        #         nums.pop(i)
-        #     else:
-        #         i += 1
-        # k = len(nums)
-        # return k
-
-        # method 2:
-        if not nums:
-            return 0
-
-        k = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]
-                k += 1
-
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                nums.pop(i)
+            else:
+                i += 1
+        k = len(nums)
         return k
+
