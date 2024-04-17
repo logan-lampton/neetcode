79154a37f11c849550091397603c60e60120d63b
logan-lampton <lampton.logan@gmail.com>
2024-04-17 11:36:24 -0400

    solved Leetcode #338

---
+++ leetcode/338_Counting_Bits.py
@@ -0,0 +1,42 @@
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+# ans[i] is the number of 1's in the binary representation of i.
+
+# Example 1:
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+
+# Example 2:
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
+
+# Constraints:
+# 0 <= n <= 105
+
+
+class Solution:
+    def countBits(self, n: int) -> [int]:
+
+        output = [0] * (n + 1)
+
+        for i in range(1, n + 1):
+            if i % 2 == 0:
+                output[i] = output[i >> 1] + 0
+            else:
+                output[i] = output[i >> 1] + 1
+
+        return output
+
+
+solution = Solution()
+print(solution.countBits(n=2))
