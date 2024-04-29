1465604e865457c62db739569eb93ec01a567beb
logan-lampton <lampton.logan@gmail.com>
2024-04-28 21:40:10 -0400

    solved Leetcode #94

---
+++ leetcode/94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,55 @@
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+
+# Example 2:
+# Input: root = []
+# Output: []
+
+# Example 3:
+# Input: root = [1]
+# Output: [1]
+
+# Constraints:
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: [TreeNode]) -> [int]:
+
+        if not root:
+            return []
+
+        stack = []
+        answer = []
+        current_node = root
+
+        while stack or current_node:
+            while current_node:
+                stack.append(current_node)
+                current_node = current_node.left
+
+            working_on_node = stack.pop()
+            answer.append(working_on_node.val)
+
+            current_node = working_on_node.right
+
+        return answer
+
+
+node3 = TreeNode(3)
+node2 = TreeNode(2, left=node3)
+node1 = TreeNode(1, right=node2)
+
+solution = Solution()
+print(solution.inorderTraversal(root=node1))
