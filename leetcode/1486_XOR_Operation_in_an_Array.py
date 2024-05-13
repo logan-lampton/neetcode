080738258fe053e08433a1a3750195968341657c
logan-lampton <lampton.logan@gmail.com>
2024-05-13 16:32:28 -0400

    solved Leetcode #1486

---
+++ leetcode/1486_XOR_Operation_in_an_Array.py
@@ -0,0 +1,36 @@
+# You are given an integer n and an integer start.
+
+# Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+# Return the bitwise XOR of all elements of nums.
+
+# Example 1:
+# Input: n = 5, start = 0
+# Output: 8
+# Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+# Where "^" corresponds to bitwise XOR operator.
+
+# Example 2:
+# Input: n = 4, start = 3
+# Output: 8
+# Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+
+# Constraints:
+# 1 <= n <= 1000
+# 0 <= start <= 1000
+# n == nums.length
+
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        nums = []
+        for i in range(n):
+            curr_number = start + 2 * i
+            nums.append(curr_number)
+        answer = 0
+        for num in nums:
+            answer ^= num
+        return answer
+
+solution = Solution()
+print(solution.xorOperation(n=5, start=0))
