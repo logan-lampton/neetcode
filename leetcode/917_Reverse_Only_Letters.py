af999d1375f234693a060545ca3583a74829ba4f
logan-lampton <lampton.logan@gmail.com>
2024-06-11 14:32:21 -0400

    solved Leetcode #917

---
+++ leetcode/917_Reverse_Only_Letters.py
@@ -0,0 +1,52 @@
+# Given a string s, reverse the string according to the following rules:
+
+# All the characters that are not English letters remain in the same position.
+# All the English letters (lowercase or uppercase) should be reversed.
+# Return s after reversing it.
+
+# Example 1:
+# Input: s = "ab-cd"
+# Output: "dc-ba"
+
+# Example 2:
+# Input: s = "a-bC-dEf-ghIj"
+# Output: "j-Ih-gfE-dCba"
+
+# Example 3:
+# Input: s = "Test1ng-Leet=code-Q!"
+# Output: "Qedo1ct-eeLg=ntse-T!"
+
+# Constraints:
+# 1 <= s.length <= 100
+# s consists of characters with ASCII values in the range [33, 122].
+# s does not contain '\"' or '\\'.
+
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        
+        output = [" "] * len(s) 
+        
+        for i in range(len(s)):
+            letter = s[i].isalpha()
+            if not letter:
+                output[i] = s[i]
+        
+        backwards_array = []
+
+        for i in range(len(s) - 1, -1, -1):
+            letter = s[i].isalpha()
+            if letter:
+                backwards_array.append(s[i])
+
+        i = 0
+        while backwards_array:
+            if output[i] == " ":
+                output[i] = backwards_array.pop(0)
+            i += 1
+
+        return "".join(output)
+
+solution = Solution()
+print(solution.reverseOnlyLetters(s = "ab-cd"))
+print(solution.reverseOnlyLetters(s = "a-bC-dEf-ghIj"))
+print(solution.reverseOnlyLetters(s = "Test1ng-Leet=code-Q!"))
