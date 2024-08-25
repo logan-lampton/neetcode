449603eec9cf753de868bf715244e85fde50cdb6
logan-lampton <lampton.logan@gmail.com>
2024-08-25 19:23:08 -0400

    solved Leetcode #2496

---
+++ leetcode/2496_Maximum_Value_of_a_String_in_an_Array.py
@@ -0,0 +1,46 @@
+# The value of an alphanumeric string can be defined as:
+
+# The numeric representation of the string in base 10, if it comprises of digits only.
+# The length of the string, otherwise.
+# Given an array strs of alphanumeric strings, return the maximum value of any string in strs.
+
+# Example 1:
+# Input: strs = ["alic3","bob","3","4","00000"]
+# Output: 5
+# Explanation: 
+# - "alic3" consists of both letters and digits, so its value is its length, i.e. 5.
+# - "bob" consists only of letters, so its value is also its length, i.e. 3.
+# - "3" consists only of digits, so its value is its numeric equivalent, i.e. 3.
+# - "4" also consists only of digits, so its value is 4.
+# - "00000" consists only of digits, so its value is 0.
+# Hence, the maximum value is 5, of "alic3".
+
+# Example 2:
+# Input: strs = ["1","01","001","0001"]
+# Output: 1
+# Explanation: 
+# Each string in the array has value 1. Hence, we return 1.
+
+# Constraints:
+# 1 <= strs.length <= 100
+# 1 <= strs[i].length <= 9
+# strs[i] consists of only lowercase English letters and digits.
+
+
+class Solution:
+    def maximumValue(self, strs: [str]) -> int:
+        max_value = 0
+        for string in strs:
+            cur_value = 0
+            if not string.isdigit():
+                cur_value = len(string)
+            else:
+                cur_value = int(string)
+            if cur_value > max_value:
+                max_value = cur_value
+        return max_value
+    
+
+solution = Solution()
+print(solution.maximumValue(strs = ["alic3","bob","3","4","00000"]))
+print(solution.maximumValue(strs = ["1","01","001","0001"]))
