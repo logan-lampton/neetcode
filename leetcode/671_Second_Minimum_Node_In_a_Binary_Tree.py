a4aa616b308b02925b8ce450e72e229dcd97bdda
logan-lampton <lampton.logan@gmail.com>
2025-01-09 18:22:07 -0500

    solved Leetcode #671

---
+++ leetcode/671_Second_Minimum_Node_In_a_Binary_Tree.py
@@ -0,0 +1,50 @@
+# Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
+
+# Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
+
+# If no such second minimum value exists, output -1 instead.
+
+# Example 1:
+# Input: root = [2,2,5,null,null,5,7]
+# Output: 5
+# Explanation: The smallest value is 2, the second smallest value is 5.
+
+# Example 2:
+# Input: root = [2,2,2]
+# Output: -1
+# Explanation: The smallest value is 2, but there isn't any second smallest value.
+
+# Constraints:
+# The number of nodes in the tree is in the range [1, 25].
+# 1 <= Node.val <= 231 - 1
+# root.val == min(root.left.val, root.right.val) for each internal node of the tree.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findSecondMinimumValue(self, root: [TreeNode]) -> int:
+        values = set()
+        stack = [root]
+
+        while stack:
+            cur_node = stack.pop()
+            values.add(cur_node.val)
+            if cur_node.left:
+                stack.append(cur_node.left)
+            if cur_node.right:
+                stack.append(cur_node.right)
+        
+        value_list = list(values)
+
+        value_list.sort()
+
+        if len(value_list) == 1:
+            return -1
+        else:
+            return value_list[1]
