67560aa5ba064745d713544f761beac87e02a0eb
logan-lampton <lampton.logan@gmail.com>
2025-01-28 16:15:32 -0500

    additional practice and new solution #155

--- leetcode/155_min_stack.py
+++ leetcode/155_min_stack.py
@@ -55,3 +55,32 @@ class MinStack(object):
         """
         :rtype: int
         """
+
+# Additional practice / different code:
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.min_stack:
+            self.min_stack.append(val)
+        elif val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        cur_val = self.stack[-1]
+        self.stack.pop()
+        if self.min_stack[-1] == cur_val:
+            self.min_stack.pop()   
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
