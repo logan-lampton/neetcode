921bba8a1eea82c61025674bb6f86bd414a389e6
logan-lampton <lampton.logan@gmail.com>
2025-02-13 15:25:21 -0500

    practice on #155

--- leetcode/155_min_stack.py
+++ leetcode/155_min_stack.py
@@ -84,3 +84,30 @@ class MinStack:
         if self.min_stack:
             return self.min_stack[-1]
         return None
+
+
+# Practice
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or self.min_stack[-1] >= val:
+            self.min_stack.append(val)
+        
+    def pop(self) -> None:
+        stack_pop = self.stack[-1]
+        self.stack.pop()
+        if stack_pop == self.min_stack[-1]:
+            self.min_stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
