70828798f46d82cf98f783f68c9e4c5abd2a2989
logan-lampton <lampton.logan@gmail.com>
2025-02-20 17:01:53 -0500

    practice and new method #1832

--- leetcode/1832_Check_if_the_Sentence_Is_Pangram.py
+++ leetcode/1832_Check_if_the_Sentence_Is_Pangram.py
@@ -24,3 +24,13 @@ def checkIfPangram(sentence: str) -> bool:
         if len(hash_map) == 26:
             return True
     return False
+
+# practice/different method:
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+        for char in alphabet:
+            if char not in sentence:
+                return False
+        return True
