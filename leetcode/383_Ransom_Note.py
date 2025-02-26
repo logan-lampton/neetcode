28bac17eeae53d89030c719483d09aa50b774dc1
logan-lampton <lampton.logan@gmail.com>
2025-02-25 19:17:41 -0500

    practice and new strategy #383

--- leetcode/383_Ransom_Note.py
+++ leetcode/383_Ransom_Note.py
@@ -27,3 +27,20 @@ def canConstruct(ransomNote: str, magazine: str) -> bool:
             return False
 
     return True
+
+# Practice:
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashmap = {}
+        for char in magazine:
+            if char not in hashmap:
+                hashmap[char] = 1
+            else:
+                hashmap[char] += 1
+        
+        for char in ransomNote:
+            if char not in hashmap or hashmap[char] == 0:
+                return False
+            hashmap[char] -= 1
+        
+        return True
