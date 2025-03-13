81edbd0531d7323dce41a38d67be2f81220df8ab
logan-lampton <lampton.logan@gmail.com>
2025-03-12 20:14:04 -0400

    practiced #1480

--- leetcode/1480_Running_Sum_of_1d_Array.py
+++ leetcode/1480_Running_Sum_of_1d_Array.py
@@ -34,3 +34,14 @@ class Solution:
             dp[i] = nums[i] + dp[i - 1]
         
         return dp
+    
+# Practice:
+class Solution:
+    def runningSum(self, nums: [int]) -> [int]:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            dp[i] = dp[i - 1] + nums[i]
+        
+        return dp
