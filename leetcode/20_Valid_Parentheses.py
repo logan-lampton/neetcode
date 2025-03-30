0d0b0b5ed3b8d7ec846096d302845810de92b0d1
logan-lampton <lampton.logan@gmail.com>
2025-03-30 19:24:31 -0400

    practiced #20

--- leetcode/20_Valid_Parentheses.py
+++ leetcode/20_Valid_Parentheses.py
@@ -66,3 +66,22 @@ class Solution:
                 stack.append(char)
         
         return not stack
+
+# Practice
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {")" : "(", "}" : "{", "]" : "["}
+
+        for char in s:
+            if char in pairs.values():
+                stack.append(char)
+            elif stack:
+                if stack[-1] == pairs[char]:
+                    stack.pop()
+                else:
+                    stack.append(char)
+            else:
+                stack.append(char)
+
+        return len(stack) == 0
