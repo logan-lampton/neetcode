49e1c20a7ea86d69303375ff1cc0981795abf473
logan-lampton <lampton.logan@gmail.com>
2025-05-02 18:56:33 -0400

    practiced #27

--- leetcode/27_Remove_Element.py
+++ leetcode/27_Remove_Element.py
@@ -33,3 +33,16 @@ class Solution:
         for i in range(len(nums) - 1, -1, -1):
             if nums[i] == "":
                 nums.pop(i)
+
+# Practice
+class Solution:
+    def removeElement(self, nums: [int], val: int) -> int:
+
+        left = 0
+
+        for right in range(len(nums)):
+            if nums[right] != val:
+                nums[left] = nums[right]
+                left += 1
+        
+        return left
