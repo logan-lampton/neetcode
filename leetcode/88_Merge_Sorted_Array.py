b3145d7a561343a0eae0ca36e03d36a38e8628ef
logan-lampton <lampton.logan@gmail.com>
2025-05-07 13:19:06 -0400

    practiced #88

--- leetcode/88_Merge_Sorted_Array.py
+++ leetcode/88_Merge_Sorted_Array.py
@@ -131,3 +131,28 @@ class Solution:
             nums1[combined_index] = nums2[nums2_index]
             nums2_index -= 1
             combined_index -= 1
+
+# Practice
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1_index = m - 1
+        nums2_index = n - 1
+
+        combined_index = m + n - 1
+
+        while nums1_index >= 0 and nums2_index >= 0:
+            if nums1[nums1_index] > nums2[nums2_index]:
+                nums1[combined_index] = nums1[nums1_index]
+                nums1_index -= 1
+            else:
+                nums1[combined_index] = nums2[nums2_index]
+                nums2_index -= 1
+            combined_index -= 1
+            
+        while nums2_index >= 0:
+            nums1[combined_index] = nums2[nums2_index]
+            nums2_index -= 1
+            combined_index -= 1
