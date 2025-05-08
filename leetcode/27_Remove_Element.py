4bd4a7c40ba12c9a947f0021eea9ebc8b5652993
logan-lampton <lampton.logan@gmail.com>
2025-05-08 17:52:38 -0400

    practiced #27

--- leetcode/27_Remove_Element.py
+++ leetcode/27_Remove_Element.py
@@ -46,3 +46,16 @@ class Solution:
                 left += 1
         
         return left
+
+# Practice
+class Solution:
+    def removeElement(self, nums: [int], val: int) -> int:
+
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
