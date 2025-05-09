3fff8da59a3a50c6c0da00be1bcf1b56d2eaee15
logan-lampton <lampton.logan@gmail.com>
2025-05-09 18:29:41 -0400

    practiced #26

--- leetcode/26_Remove_Duplicates_from_Sorted_Array.py
+++ leetcode/26_Remove_Duplicates_from_Sorted_Array.py
@@ -66,3 +66,17 @@ class Solution:
                 insert_index += 1
         
         return insert_index
+
+# Practice
+class Solution:
+    def removeDuplicates(self, nums: [int]) -> int:
+        if len(nums) == 1:
+            return 1
+        k = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        
+        return k
