d674a71074a09d89ea36755606ca44f89d2516a3
logan-lampton <lampton.logan@gmail.com>
2025-05-17 14:03:56 -0400

    practiced #392

--- leetcode/392_Is_Subsequence.py
+++ leetcode/392_Is_Subsequence.py
@@ -28,3 +28,20 @@ class Solution:
             right += 1
         # if left pointer made it all the way through the length of list s, then we return True, False if left != len(s)
         return left == len(s)
+
+
+# Practice:
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        
+        s_index = 0
+
+        for i in range(len(t)):
+            if t[i] == s[s_index]:
+                s_index += 1
+                if s_index == len(s):
+                    return True
+        
+        return False
