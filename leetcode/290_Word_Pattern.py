0066c6c7022f2ced92f7ce3d054a801037496401
logan-lampton <lampton.logan@gmail.com>
2025-05-20 16:53:00 -0400

    practiced #290

--- leetcode/290_Word_Pattern.py
+++ leetcode/290_Word_Pattern.py
@@ -43,3 +43,30 @@ class Solution:
                 if pattern_dict[pattern[i]] != s_split[i]:
                     return False
         return True
+  
+
+# Practice:
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+
+        if len(words) != len(pattern):
+            return False
+
+        char_to_word = {}
+        word_to_char = {}
+
+        for char, word in zip(pattern, words):
+            if char in char_to_word:
+                if char_to_word[char] != word:
+                    return False
+            else:
+                char_to_word[char] = word
+        
+            if word in word_to_char:
+                if word_to_char[word] != char:
+                    return False
+            else:
+                word_to_char[word] = char
+        
+        return True
